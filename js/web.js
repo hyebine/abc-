398789d8d7dab8fee66686454a0cdd14ff8591dc
hyebine <berryme015@gmail.com>
2023-11-03 17:53:14 +0900

    A형 스와이퍼 자바스크립트 완료

--- js/web.js
+++ js/web.js
@@ -1,4 +1,11 @@
-$(document).ready(function () {
-    //화면이 준비되면 실행해라.
+$(document).ready(function () { /*실전에선 안쓰임 시험때문에 만듦 */
+    setInterval(function () {
+        $(".swiper-wrapper").animate({ "marginLeft": -1200 }, 400, function () {
+            //왼쪽으로 1200만큼 가라 400이 디폴트값
+            $("div:first-child", this).appendTo($(this))
+            //this안에 div 첫째가 막내가 됨
+            $(this).css("marginLeft", 0)
+        })
+    }, 3000) //3초마다 실행
 
 })
