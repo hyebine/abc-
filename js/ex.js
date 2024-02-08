ccecadc74ec724576ab4a05c1295a2b155a7c502
hyebine <berryme015@gmail.com>
2024-02-08 12:19:17 +0900

    좌 슬라이드

--- js/ex.js
+++ js/ex.js
@@ -1,5 +1,14 @@
-$(function () {
-  $('.swiper-wapper').animate({ "marginLeft" : -800 }, 300, function(){
+$(function(){
 
-  })
+  setInterval(anifun, 3000)
+
+  function anifun(){
+        $(".swiper-wapper").animate({
+          "marginLeft" : -$(".swiper-slide").width() 
+        }, 400, function(){
+          $(".swiper-wapper .swiper-slide").eq(0).appendTo($(".swiper-wapper"))
+          $(".swiper-wapper").css("margin-left" , 0)
+
+        })
+    }
 })
