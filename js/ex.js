301f885c0185d8e65a9d1d977dae595aa146eba8
hyebine <berryme015@gmail.com>
2024-02-08 12:52:25 +0900

    fade 슬라이드

--- js/ex.js
+++ js/ex.js
@@ -1,14 +1,25 @@
-$(function(){
+$(function () {
 
-  setInterval(anifun, 3000)
+  let count = 0; // 한번만 선언
 
-  function anifun(){
-        $(".swiper-wapper").animate({
-          "marginTop" : -$(".swiper").height() 
-        }, 400, function(){
-          $(".swiper-wapper .swiper-slide").eq(0).appendTo($(".swiper-wapper"))
-          $(".swiper-wapper").css({"marginTop" : 0})
+  setInterval(fadefun, 3000)
 
-        })
-    }
+  // function anifun(){
+  //       $(".swiper-wapper").animate({
+  //         "marginTop" : -$(".swiper").height() 
+  //       }, 400, function(){
+  //         $(".swiper-wapper .swiper-slide").eq(0).appendTo($(".swiper-wapper"))
+  //         $(".swiper-wapper").css({"marginTop" : 0})
+
+  //       })
+  //   }
+
+  function fadefun() {
+
+    count++;
+    count %= $(".swiper-slide").length;
+
+    $(".swiper-slide").eq(count).addClass("active").sliblings().removeClass("active");
+  }
+
 })
